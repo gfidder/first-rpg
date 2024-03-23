a71d605ac45f264f576bb886b126c01105549948
Gabe Fidder <gabfid@gmail.com>
2024-03-23 00:46:28 -0500

    trying out ldtk

--- first_rpg_game/src/main.rs
+++ first_rpg_game/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::window::{PresentMode, WindowMode, WindowPlugin};
+use bevy_ecs_ldtk::{prelude, LdtkPlugin, LdtkWorldBundle};
 
 fn main() {
     App::new()
@@ -12,5 +13,20 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .run()
+        .add_plugins(LdtkPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 2.0;
+    camera.transform.translation.x += 1280.0 / 4.0;
+    camera.transform.translation.y += 720.0 / 4.0;
+    commands.spawn(camera);
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("tile-based-game.ldtk"),
+        ..Default::default()
+    });
 }
